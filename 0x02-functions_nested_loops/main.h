3f1365c5a885c1fde397fd39edba5753ef2a016b
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-15 07:25:19 +0000

    Updated main header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,5 +12,8 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 void print_times_table(int n);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 
 #endif /* _MAIN_H_ */
