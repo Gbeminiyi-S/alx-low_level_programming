388e17f027b4af8a7d8434fc99afb21cdd4d5118
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-15 16:47:03 +0000

    Task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer
+ */
+void print_number(int n)
+{
+	unsigned int power, temp;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	temp = n;
+
+	power = power_number(n);
+
+	while (power != 1)
+	{
+		_putchar(((temp / power) % 10) + ('0' - 0));
+		power /= 10;
+	}
+	_putchar((temp % 10) + ('0' - 0));
+}
+/**
+ * power_number - finds the multiple of 10 of an integer
+ * @n: integer
+ *
+ * Return: result
+ */
+int power_number(int n)
+{
+	int power = 1;
+
+	while (n > 9)
+	{
+		n /= 10;
+		power *= 10;
+	}
+	return (power);
+}
