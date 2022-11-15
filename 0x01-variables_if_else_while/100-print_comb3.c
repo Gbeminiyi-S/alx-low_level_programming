c18bdbc5cccb70e11542314106cc696327107772
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-15 02:02:55 +0000

    correcting the output length of chars

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,17 @@
 int main(void)
 {
 	int tens, unit;
-	int comma, space;
+	int comma = 0, space = 0;
 
 	for (tens = 0; tens < 9; tens++)
 	{
 		for (unit = 1 + tens; unit < 10; unit++)
 		{
-			putchar(comma);
-			putchar(space);
+			if (comma != 0 && space != 0)
+			{
+				putchar(comma);
+				putchar(space);
+			}
 			putchar(tens + ('0' - 0));
 			putchar(unit + ('0' - 0));
 			comma = 44, space = 32;	/* ASCII: 44 - ',' | 32 - SPC */
