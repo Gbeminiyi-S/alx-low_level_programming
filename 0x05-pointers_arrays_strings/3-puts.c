2c9300a8b3fbd3cbc2020c3013c149ad4daa01ec
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-15 17:34:21 +0000

    updated file descriptor to STDOUT

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,7 +8,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		write(STDERR_FILENO, str++, 1);
+		write(STDOUT_FILENO, str++, 1);
 	}
-	write(STDERR_FILENO, "\n", 1);
+	write(STDOUT_FILENO, "\n", 1);
 }
