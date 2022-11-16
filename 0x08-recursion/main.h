9a33b9b739fdc8bcdce468191cff742dd898d7d5
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2022-11-16 19:12:49 +0100

    updated with new prototypes

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,21 +3,17 @@
 
 #include <stdio.h>
 
-int factorial(int n);
-int _pow_recursion(int x, int y);
-void _puts_recursion(char *s);
 int _putchar(char c);
+void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int _strlen(char *s);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-char *test(char *s5, char *s6, char *t5, char *t6);
-char *wildcard(char *s3, char *s4);
 int _recursion_calc(int n, int num);
+int is_prime_number(int n);
 int _recursion_check(int n, int num);
+int is_palindrome(char *s);
 int palindrome_check(char *s, int len, int i);
 int _strlen(char *s);
 
