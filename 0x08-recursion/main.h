3187ff486ae937d59fa2506ce72d393f257b6442
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-16 15:44:02 +0000

    updated main header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,26 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+int factorial(int n);
+int _pow_recursion(int x, int y);
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int _sqrt_recursion(int n);
+int ifsquare(int i, int j);
+int is_prime_number(int n);
+int prime_check(int i, int j); 
+int primeCheck(int i, int j);
+int is_prime_number(int n);
+int str_length(char *s);
+int check(char *s, int length, int i);
+int is_palindrome(char *s);
+int str_length(char *s);
+int wildcmp(char *s1, char *s2);
+char *test(char *s5, char *s6, char *t5, char *t6);
+char *wildcard(char *s3, char *s4);
+
+#endif /* _MAIN_H_ */
