42796a834f058d873fff4ab51ea6322f80b79c56
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-16 11:44:33 +0000

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (*s1 - *s2);
+	int diff = 0;
+
+	while (s1 || s2)
+	{
+		diff = *s1 - *s2;
+
+		if (diff || !*s1 || !*s2)
+			break;
+
+		s1++, s2++;
+	}
+	return (diff);
 }
