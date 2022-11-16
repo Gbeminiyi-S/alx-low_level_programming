d40868ceb7eb18dc0a94c4a2b0a6c39cde4c3692
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-16 10:24:39 +0000

    updfated main header file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
+char *_strncat(char *dest, char *src, int n);
+void print_number(int n);
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+int strcount(char str[]);
+char *rot13(char *str);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+void strUpdate(char *buffer, int size);
+
+#endif /* _MAIN_H_ */
