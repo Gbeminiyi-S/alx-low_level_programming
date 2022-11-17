d0abc76bebb2c74238cedb4d9ee1ae0405543247
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-17 01:53:43 +0000

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @ac: argument count
+ * @av: double pointer to argument
+ *
+ * Return: 0 Always
+ */
+int main(int ac, char **av __attribute__((unused)))
+{
+	printf("%i\n", ac - 1);
+
+	return (0);
+}
