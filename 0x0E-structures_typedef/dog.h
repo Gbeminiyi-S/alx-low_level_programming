9ef56c285092724e4e833f3ce37078beac64eb1e
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-17 18:19:15 +0000

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - defines a variable of type struct dog
+ * @name: pointer to Dog's name
+ * @age: dog's age
+ * @owner: pointer to dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
