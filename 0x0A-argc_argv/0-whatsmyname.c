10389b503226b77f21e55068c7a565f77fb1b0b9
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-17 01:50:32 +0000

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name
+ * @ac: argument count
+ * @av: double pointer to argument
+ *
+ * Return: 0 Always
+ */
+int main(int ac __attribute__((unused)), char **av)
+{
+	printf("%s\n", av[0]);
+
+	return (0);
+}
