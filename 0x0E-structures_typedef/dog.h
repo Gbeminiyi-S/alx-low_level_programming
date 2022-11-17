369cf84165d88f4dcf3db159c9daef506e146177
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-11-17 19:06:20 +0000

    updated header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - defines a variable of type struct dog
  * @name: pointer to Dog's name
@@ -13,5 +16,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *str);
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_ */
