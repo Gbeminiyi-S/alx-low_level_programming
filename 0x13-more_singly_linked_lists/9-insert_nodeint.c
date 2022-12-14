a536d2f4a29a440586de456374a7d3cca758af4d
Gbeminiyi2022 <samagbeyigbeminiyi@gmail.com>
2022-12-14 12:48:38 +0000

    Update for empty head and idx == 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,52 +1,41 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position
- * @head: double pointer to the start of list
- * @idx: is the index of the list where the new node should be added
- * @n: new node data
- *
- * Description: if it is not possible to add the new node at index idx,
- *			do not add the new node and return NULL
- * Return: the address of the new node, else NULL
+ * insert_nodeint_at_index -  inserts a new node at a given position
+ * @head: double pointer to the linked list;
+ * @idx: index of the list where the new node should be added
+ * @n: node value
+ * Return: address of the new node, else, NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp = NULL, *new_node = NULL;
+	listint_t *node = malloc(sizeof(listint_t));
+	listint_t *ptr = *head;
 
-	if (*head || (!(*head) && idx == 0))
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->next = NULL;
+
+	if (*head == NULL)
+		*head = node;
+	else if (idx == 0)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (!new_node)
+		node->next = *head;
+		*head = node;
+	}
+	else if (idx > 0)
+	{
+		while (idx != 1)
 		{
-			return (NULL);
-		}
-		new_node->n = n;
-		new_node->next = NULL;
-
-		if (!(*head) && idx == 0)
-			return new_node;
-
-		temp = *head;
-		if (idx == 0)
-		{
-			new_node->next = *head;
-			*head = new_node;
-			return (new_node);
-		}
-
-		while (idx != 1 && temp)
-		{
-			temp = temp->next;
+			if (ptr == NULL)
+				return (NULL);
+			ptr = ptr->next;
 			idx--;
 		}
-		if (idx == 1)
-		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
-		}
+		node->next = ptr->next;
+		ptr->next = node;
 	}
-	return (NULL);
-}
 
+	return (node);
+}
