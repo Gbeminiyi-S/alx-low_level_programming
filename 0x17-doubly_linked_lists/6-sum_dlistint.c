cd3ba5f0e721fb80bf767f7ddab1af1a22340194
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-13 02:31:05 +0000

    Task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sums of all the data (n) of a dlistint_t list
+ * @head: pointer to the list's head
+ *
+ * Return: sum, else, 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+	{
+		return (0);
+	}
+
+	while (head)
+	{
+		head = head->next;
+		if (head)
+			sum += head->n;
+	}
+	return (sum);
+}
