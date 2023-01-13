a2f508aa882df4d81957177eac574abcddcbfe59
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-13 01:42:41 +0000

    Task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,14 +20,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	temp->n = n;
 	temp->next = *head;
 
-	if (!(*head))
+	if (*head)
 	{
-		*head = temp;
+		(*head)->prev = temp;
 	}
-	else
-	{
-		*head = temp;
-	}
-
+	*head = temp;
 	return (temp);
 }
