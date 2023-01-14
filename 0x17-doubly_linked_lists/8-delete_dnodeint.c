489fc15075aa88034cd1900afa00169f120573f2
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-14 14:36:23 +0000

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -20,7 +20,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			if ((*head)->next)
 			{	
 				*head = (*head)->next;
-				*head->prev = NULL;
+				(*head)->prev = NULL;
 				free(ptr);
 			}
 			else
