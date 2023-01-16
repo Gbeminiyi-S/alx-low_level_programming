5656c993e55fe9e27d3fe41c86cee12c623da4a4
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-16 07:04:46 +0000

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,48 +3,48 @@
  * print_listint_safe - prints a listint_t linked list
  * @head: pointer to start of the list
  *
- * Return: number of nodes in the list, else, exit(98)
+ * Return: number of nodes in the list, else, exit(98)i
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slow = head, *fast = head, *temp = head;
-	size_t isLoop = 0, i = 0;
+	const listint_t *tortoise = head, *hare = head, *temp = head;
+	size_t isLoop = 0, nodes = 0;
 
 	if (!head)
-	{
-		printf("0\n");
-		exit(98);
-	}
-	while (slow && fast && fast->next)
+		printf("0\n"), exit(98);
+	while (tortoise && hare && hare->next)
 	{	/* Detecting if linked list is looped */
-		slow = slow->next, fast  = fast->next->next;
-		if (slow == fast)
+		tortoise = tortoise->next, hare  = hare->next->next;
+		if (tortoise == hare)
 		{
-			slow = slow->next, isLoop = 1;
+			isLoop = 1;
 			break;
 		}
 	}
-	if (!isLoop) /* if linked list is not looped */
+	/* if linked list is not looped */
+	if (!isLoop)
 	{
 		while (temp)
 		{
 			printf("[%p] %d\n", (void *)temp, temp->n);
-			temp = temp->next;
-			i++;
+			temp = temp->next, nodes++;
 		}
-		return (i);
+		return (nodes);
 	}
-	while (temp)
-	{	/* if linked list is looped */
-		i++;
+	/* if linked list is looped */
+	while (temp != tortoise)
+	{
 		printf("[%p] %d\n", (void *)temp, temp->n);
-		if (slow == temp)
-		{
-			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
-			return (i);
-		}
-		else
-			temp = temp->next;
+		temp = temp->next, nodes++;
 	}
-	exit(98);
+	tortoise = head;
+	while (tortoise != hare)
+	{
+		tortoise = tortoise->next;
+		printf("[%p] %d\n", (void *)hare, hare->n);
+		hare = hare->next, nodes++;
+		if (tortoise == hare)
+			printf("-> [%p] %d\n", (void *)hare, hare->n);
+	}
+	return (nodes);
 }
