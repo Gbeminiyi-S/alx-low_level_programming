4eab9321f1946d199aebd56aa0d7c820ad767f57
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-16 07:53:23 +0000

    Task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,15 +12,7 @@ size_t print_listint_safe(const listint_t *head)
 
 	if (!head)
 		printf("0\n"), exit(98);
-	while (tortoise && hare && hare->next)
-	{	/* Detecting if linked list is looped */
-		tortoise = tortoise->next, hare  = hare->next->next;
-		if (tortoise == hare)
-		{
-			isLoop = 1;
-			break;
-		}
-	}
+	isLoop = loopCheck(head);
 	/* if linked list is not looped */
 	if (!isLoop)
 	{
@@ -32,19 +24,47 @@ size_t print_listint_safe(const listint_t *head)
 		return (nodes);
 	}
 	/* if linked list is looped */
-	while (temp != tortoise)
+	while (1)
 	{
 		printf("[%p] %d\n", (void *)temp, temp->n);
 		temp = temp->next, nodes++;
-	}
-	tortoise = head;
-	while (tortoise != hare)
-	{
-		tortoise = tortoise->next;
-		printf("[%p] %d\n", (void *)hare, hare->n);
-		hare = hare->next, nodes++;
+		if (temp == tortoise)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				printf("[%p] %d\n", (void *)hare, hare->n);
+				hare = hare->next, nodes++;
+			}
+		}
+		else
+			continue;
 		if (tortoise == hare)
+		{
 			printf("-> [%p] %d\n", (void *)hare, hare->n);
+			break;
+		}
 	}
 	return (nodes);
+}
+/**
+ * loopCheck - Checks if loop is present in list
+ * @head: pointer to list's head
+ *
+ * Return: i if looped, else, 0
+ */
+size_t loopCheck(const listint_t *head)
+{
+	const listint_t *hare = head, *tortoise = head;
+
+	while (tortoise && hare && hare->next)
+	{	/* Detecting if linked list is looped */
+		tortoise = tortoise->next, hare  = hare->next->next;
+		if (tortoise == hare)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
