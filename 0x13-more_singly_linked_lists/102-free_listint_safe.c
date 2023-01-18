18f19fbd712de5e4226aaa066c2f88687d0d6a77
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-18 16:39:33 +0000

    Task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t linked list
+ * @h: double pointer to start of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *tortoise = *h, *hare = *h, *temp = *h;
+	size_t isLoop = 0, nodes = 0;
+
+	if (!(*h))
+	{
+		return (nodes);
+	}
+	while (tortoise && hare && hare->next)
+	{
+		/* Detecting if linked list is looped */
+		tortoise = tortoise->next, hare = hare->next->next;
+		if (tortoise == hare)
+		{
+			isLoop = 1;
+			break;
+		}
+	}
+	/* Removing the loop*/
+	if (isLoop)
+	{
+		tortoise = *h;
+		while (tortoise != hare)
+		{
+			tortoise = tortoise->next;
+			hare = hare->next, nodes++;
+		}
+		hare->next = NULL;
+	}
+	while (*h)
+	{
+		temp = *h;
+		*h = (*h)->next;
+		free(temp);
+		nodes++;
+	}
+	return (nodes);
+}
