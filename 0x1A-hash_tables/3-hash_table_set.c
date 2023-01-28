6c36804b5e3dd898ebee79ee24e33169df4e879b
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-28 13:22:14 +0000

    update to update existing key's value

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,12 +16,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *new_node = NULL;
+	char *value_copy = NULL;
 
 	if (!ht || !key || !value)
 		return (0);
-
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
 	/* Get the key's index from the key_index function*/
 	index = key_index((const unsigned char *)key, ht->size);
+	/* where the key already exists */
+	if (ht->array[index] && *(ht->array[index]->key) == *key)
+	{
+		free(ht->array[index]->value);
+		ht->array[index]->value = value_copy;
+		return (1);
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
@@ -32,15 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
-	new_node->value = strdup(value);
-	if (!new_node->value)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
-
+	new_node->value = value_copy;
 	new_node->next = NULL;
 
 	/* adding the new node at the beginning of array */
