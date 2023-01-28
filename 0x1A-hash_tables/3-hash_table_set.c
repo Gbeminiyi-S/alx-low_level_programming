302c431115593042e73e57b96fb3cc03c78791bf
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-28 12:37:31 +0000

    update to remove extraneous element

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
-		free(value_copy);
 		return (0);
 	}
 	new_node->key = strdup(key);
@@ -39,6 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	if (!new_node->value)
 	{
+		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
