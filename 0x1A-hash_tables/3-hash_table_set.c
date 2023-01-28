cac9c2895664c83497a57679c13df8735d19d2ac
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-28 12:35:57 +0000

    Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: is the key
+ * @value: is the value associated with the key
+ *
+ * Description:
+ *	key can not be an empty string
+ *	value must be duplicated. value can be an empty string
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node = NULL;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	/* Get the key's index from the key_index function*/
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = strdup(value);
+	if (!new_node->value)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->next = NULL;
+
+	/* adding the new node at the beginning of array */
+	if (ht->array[index])
+	{
+		new_node->next = ht->array[index];
+	}
+	ht->array[index] = new_node;
+	return (1);
+}
