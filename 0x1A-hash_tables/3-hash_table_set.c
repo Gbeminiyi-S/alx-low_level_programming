fef55df98f537145afbd0b399c3587567e95ec77
Gbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2023-01-28 12:59:45 +0000

    update to add linked list for collision

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -47,6 +47,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht->array[index])
 		ht->array[index] = new_node;
 	else
+	{
+		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
+	}
 	return (1);
 }
