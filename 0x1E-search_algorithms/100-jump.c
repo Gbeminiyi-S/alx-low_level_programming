0d817102bc3e4a08078b0ccafbebb738f0ab8a2a
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-04-16 18:08:16 +0100

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -41,7 +41,7 @@ int jump_search(int *array, size_t size, int value)
                 if (step > size - 1)
                         break;
         }
-        printf("Value found between indexes [%ld] and [%ld]\n", prev, step);
+        printf("Value found between indexes [%ld] and [%ld]\n", prev, (prev += sqrt(size)));
 
         /* linear search */
         while ((prev <= min(step, size - 1)) && (array[prev] <= value))
