e3269d793072c4587d8cea27ec829adde499236d
Gbeminiyi Samagbeyi <111027908+Gbeminiyi-S@users.noreply.github.com>
2023-04-16 18:06:32 +0100

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -29,6 +29,9 @@ int jump_search(int *array, size_t size, int value)
 	if (!array || !size)
 		return (-1);
 
+	if (array[min(step, size - 1)] > value)
+                printf("Value checked array[%ld] = [%d]\n", step, array[step]);
+
         /* find the block that may contain the value */
         while (array[min(step, size - 1)] < value)
         {
