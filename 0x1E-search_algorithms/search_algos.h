c44a702bb4db3153d76547e6074b5ef3309b05a0
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-04-20 14:27:08 +0000

    Task 10 - exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,6 @@ void print_array(int *array, size_t low, size_t high);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int interpolation(int *array, size_t low, size_t high, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
