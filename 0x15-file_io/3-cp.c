d177c1af0cf3581e0f891fa98b680a57ebb4474b
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-12 01:27:41 +0000

    Task 3 - update to reformat code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,6 +12,18 @@ void error(void)
 	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 }
 
+/**
+ * main - Copies the contents of a file to another file
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 0 on success
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ *              If file_from does not exist or cannot be read - exit code 98.
+ *              If file_to cannot be created or written to - exit code 99.
+ *              If file_to or file_from cannot be closed - exit code 100.
+ */
 int main(int ac, char *av[])
 {
 	int from_file, to_file, buflen;
