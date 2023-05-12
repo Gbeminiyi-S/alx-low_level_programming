938b8a83fc2dd96bd9abaf2a8608ece07a0002fb
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-05-12 01:15:44 +0000

    Task 3 - update to reformat code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,34 +3,37 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+
+/**
+ * error - Error message for a condition
+ */
+void error(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+}
+
 int main(int ac, char *av[])
 {
-	int from_file, to_file, buflen, close_file, val;
+	int from_file, to_file, buflen;
 	char buf[1024];
 
 	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		error(), exit(97);
 	from_file = open(av[1], O_RDONLY);
 	if (from_file == -1)
 	{
-                write(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-                exit(98);
-        }	
-
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	to_file = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to_file == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(from_file), exit(99);
 	}
-	while((buflen = read(from_file, buf, 1024)) > 0)
+	while ((buflen = read(from_file, buf, 1024)) > 0)
 	{
-    		val = write(to_file, buf, buflen);
-		if (val == -1)
+		if (write(to_file, buf, buflen) == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(from_file), exit(99);
@@ -38,19 +41,14 @@ int main(int ac, char *av[])
 	}
 	if (buflen == -1)
 	{
-        	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-               	exit(98);
-        }
-
-	close_file = close(from_file);
-	if (close_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_file);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
 	}
-
-	close_file = close(to_file);
-	if (close_file == -1)
+	if (close(from_file) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from_file), exit(100);
+	}
+	if (close(to_file) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to_file);
 		exit(100);
